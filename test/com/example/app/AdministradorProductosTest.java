550b4d52b6913d1a609b76ddf1cffa32ada47d3c
leomartinez2020 <lowenhard@gmail.com>
2022-03-31 09:20:24 -0500

    Realizar test unitario

--- test/com/example/app/AdministradorProductosTest.java
+++ test/com/example/app/AdministradorProductosTest.java
@@ -9,8 +9,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AdministradorProductosTest {
     @Test
     public void main() {
-        Producto p = new Bebida(104, "Té", 1800);
-        assertEquals(p.getDescuento(), 0.0);
+        Producto p1 = new Bebida(101, "Gaseosa", 1500);
+        assertEquals(p1.getDescuento(), 0.0);
+        Producto p2 = new Bebida(102, "Cerveza", 3000);
+        assertEquals(p2.getDescuento(), 0.0);
+        assertEquals(getDescuento(4500), 900);
+    }
+
+    public double getDescuento(double precio) {
+        return precio * 0.2d;
     }
 }
 
